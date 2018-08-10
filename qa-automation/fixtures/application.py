9b1b6b388c54752454d4fd94cdb264faa194c102
Daer <daer92@gmail.com>
2018-08-10 12:11:52 +0300

    user create fixes

--- qa-automation/fixtures/application.py
+++ qa-automation/fixtures/application.py
@@ -16,10 +16,10 @@ class Application:
     def login_process(self, User_Data):
         wd = self.wd
         wd.find_element_by_name("mxEmail").click()
-        #wd.find_element_by_name("mxEmail").clear()
+        wd.find_element_by_name("mxEmail").clear()
         wd.find_element_by_name("mxEmail").send_keys(User_Data.username)
         wd.find_element_by_name("password").click()
-        #wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").clear()
         wd.find_element_by_name("password").send_keys(User_Data.password_key)
         wd.find_element_by_xpath("/html/body/div[2]/div/div/form/button").click()
         time.sleep(5)
@@ -28,35 +28,60 @@ class Application:
         wd = self.wd
         wd.get("https://stg.mercaux.com/#/login")
 
-    def add_new_user(self, NewUserData):
-        wd = self.wd
 
-        #Прописать роль авто тестинга
+    def add_new_user_select(self):
+        wd = self.wd
+        #Path to adding user
         wd.find_element_by_xpath('//*[@id="wrapper"]/div[1]/nav/ul/li[6]/a').click()
         wd.find_element_by_xpath('//*[@id="wrapper"]/div[1]/nav/ul/li[6]/ul/li[2]/a').click()
         wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div[1]/div/div/button').click()
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[1]/div/input').click()
-        time.sleep(1)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[1]/div/input').send_keys(NewUserData.first_name)
-        time.sleep(4)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[2]/div/input').click()
-        time.sleep(4)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[2]/div/input').send_keys(NewUserData.last_name)
-        time.sleep(4)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[3]/div/input').click()
-        time.sleep(4)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[3]/div/input').send_keys(NewUserData.new_email)
-        time.sleep(4)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[4]/div/input').click()
-        time.sleep(4)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[4]/div/input').send_keys(NewUserData.new_password)
-        time.sleep(4)
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[5]/div/select').click()
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[5]/div/select/option[6]').click()
-        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[7]/div/select').click()
+        #Country selector
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[6]/div/select').click()
         wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[6]/div/select/option[3]').click()
+        # Tap to side
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[1]').click()
+        # Role selector
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[5]/div/select').click()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[5]/div/select/option[15]').click()
+        # Tap to side
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[1]').click()
+
+
+    def add_new_user_input(self, NewUserData):
+        wd = self.wd
+        #Password
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[4]/div/input').click()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[4]/div/input').clear()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[4]/div/input').send_keys(NewUserData.new_password)
+        # Tap to side
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[1]').click()
+        time.sleep(1)
+        #E-mail
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[3]/div/input').click()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[3]/div/input').clear()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[3]/div/input').send_keys(NewUserData.new_email)
+        # Tap to side
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[1]').click()
+        #Last name
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[2]/div/input').click()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[2]/div/input').clear()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[2]/div/input').send_keys(NewUserData.last_name)
+        # Tap to side
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[1]').click()
+        #First name
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[1]/div/input').click()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[1]/div/input').clear()
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[1]/div/input').send_keys(NewUserData.first_name)
+        #Tap to side
+        wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[1]').click()
+        #save button
         wd.find_element_by_xpath('//*[@id="page-wrapper"]/div/div/div/div/div[2]/form/div[9]/div[2]/div/button[2]').click()
-        time.sleep(6)
+        time.sleep(5)
+
+
+
+
+
 
 
     def delete_user(self):
--- qa-automation/tests/add_new_user.py
+++ qa-automation/tests/add_new_user.py
@@ -1,6 +1,7 @@
 from fixtures.application import Application
 from fixtures.userdata import NewUserData, UserData
 import pytest
+from datetime import time
 
 
 @pytest.fixture
@@ -10,13 +11,12 @@ def app(request):
     return fixture
 
 
-def test_add_user(app):
+def test_add_new_user(app):
     app.open_home_page()
     app.login_process(UserData(username="ilya-benetton@mercaux.com", password_key="123321"))
-    app.add_new_user(NewUserData(first_name="Auto1222", last_name="Test", new_email="auto@test212.ru", new_password="123"))
+    app.add_new_user_select()
+    app.add_new_user_input(NewUserData(first_name="Auto1222", last_name="Test", new_email="auto@test123.ru", new_password="123"))
 
 
-#def test_delete_user(app):
-    #app.open_home_page()
-    #app.login_process(UserData(username="ilya-benetton@mercaux.com", password_key="123321"))
-    #app.delete_user()
+
+#
